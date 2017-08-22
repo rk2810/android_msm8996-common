5497975dad3a4e7c27037fed400b40434c6f98bf
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:11:27 +0200

    msm8996-common: Build Camera HAL

--- msm8996.mk
+++ msm8996.mk
@@ -124,7 +124,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap
+    Snap \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl
 
 # CNE
 PRODUCT_PACKAGES += \
