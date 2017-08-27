b7723b0d71021c3fc3c01b83eda2d591bfb36cbc
rk2810 <rohan.kumquat@gmail.com>
2017-08-27 06:29:25 +0000

    leave sepolicies for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -173,9 +173,9 @@ PROTOBUF_SUPPORTED := true
 TARGET_RIL_VARIANT := caf
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Vendor init
 TARGET_INIT_VENDOR_LIB := libinit_msm8996
