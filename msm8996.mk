950846e62291dfd0cd16b821cc728b13231e9619
rk2810 <rohan.kumquat@gmail.com>
2017-08-30 14:28:15 +0000

    Shit happens

--- msm8996.mk
+++ msm8996.mk
@@ -159,7 +159,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     gralloc.msm8996 \
-#    hwcomposer.msm8996 \
+    hwcomposer.msm8996 \
     libgenlock \
     memtrack.msm8996
 
