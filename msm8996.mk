fa86721768d3b989548d6bf0ed35dd4f841e128a
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-09-05 01:37:22 +0200

    Handle AOSP HALs in a different way Restore QCOM flags and set specific AOSP path for display and media projects
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- msm8996.mk
+++ msm8996.mk
@@ -84,23 +84,20 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    audiod \
+    audio.a2dp.default \
     audio.primary.msm8996 \
     audio.a2dp.default \
     audio.usb.default \
     audio.r_submix.default \
     libaudio-resampler \
-
-PRODUCT_PACKAGES += \
+    libaudioroute \
+    libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libqcomvoiceprocessingdescriptors \
-    libqcompostprocbundle
-
-PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-impl \
-    android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    libtinycompress \
+    libvolumelistener \
+    tinymix
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
