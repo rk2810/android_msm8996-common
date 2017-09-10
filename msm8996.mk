6aeb5bc176d0921c6bf86db982d49b004083360c
rk2810 <rohan.kumquat@gmail.com>
2017-09-10 11:10:58 +0000

    more test before a clean tree

--- msm8996.mk
+++ msm8996.mk
@@ -84,8 +84,6 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-#    audiod \
-    audio.a2dp.default \
     audio.primary.msm8996 \
     audio.a2dp.default \
     audio.usb.default \
@@ -102,14 +100,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
-    libaudioroute \
-    libqcompostprocbundle \
-    libqcomvisualizer \
-    libqcomvoiceprocessing \
-    libtinycompress \
-    libvolumelistener \
-    tinymix
+    android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
